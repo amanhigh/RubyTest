8bf710d4b3ab5d19a62c902e0b95f85b6409565b
Aman <coool.aman@gmail.com>
2011-10-27 13:56:54 +0530

    New RubyTest Project Created using rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyTest::Application.config.secret_token = 'c0c0468c403d79f4df22841d651a26295fad4b631df054075893d986b8a614dec497b72a1bcc028f16e024417172cf115dfbd4838450c38552e6479e4f2626ba'
