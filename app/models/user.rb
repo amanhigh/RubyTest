31329bb5b379d064699e4f6d33b695b45b8655f4
Amanpreet Singh <amanpreet.singh@flipkart.com>
2014-01-04 22:16:22 +0530

    Added password & encrypted password.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessor :password
+  attr_accessible :email, :name, :password, :password_confirmation
 
   validates :name, :presence => true, :length => {:maximum => 50}
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, :presence => true, format: {:with => VALID_EMAIL_REGEX}, :uniqueness => {:case_sensitive => false}
+
+  validates :password, :presence => true, :confirmation => true, :length => {:within => 6..40}
 end
