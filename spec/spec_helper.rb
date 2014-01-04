fd03837f442ff2ea5cf7a8a5a54dbbed85e0e8b6
Amanpreet Singh <amanpreet.singh@flipkart.com>
2014-01-04 19:49:32 +0530

    Moved from spork to zeus.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
+# This file is copied to s†pec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
@@ -24,37 +24,4 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
-end
-
-Spork.each_run do
-  # allows capybara JS tests to run in separate thread
-  class ActiveRecord::Base
-    mattr_accessor :shared_connection
-    @@shared_connection = nil
-
-    def self.connection
-      @@shared_connection || retrieve_connection
-    end
-  end
-
-  # Forces all threads to share the same connection. This works on
-  # Capybara because it starts the web server in a thread.
-  ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
-
-  ActiveSupport::Dependencies.clear
-  ActiveRecord::Base.instantiate_observers
-
-  Dir[File.join(File.dirname(__FILE__), '..', 'app', 'helpers', '*.rb')].each do |file|
-    require file
-  end
-
-  # This code will be run each time you run your specs.
-  load "#{Rails.root}/config/routes.rb"
-  #FactoryGirl.reload
-  # reload all the models
-  Dir["#{Rails.root}/app/models/**/*.rb"].each do |model|
-    load model
-  end
-
-
 end
