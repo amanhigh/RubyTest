9185f5b210b9bc604c64e315ca58ee17896760e7
Amanpreet Singh <amanpreet.singh@flipkart.com>
2014-01-04 23:40:15 +0530

    Added user factory & tests for show page.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,8 +3,25 @@ require 'spec_helper'
 describe UsersController do
   render_views
 
+  describe "GET 'show'" do
+
+    before(:each) do
+      @user = FactoryGirl.create(:user)
+    end
+
+    it 'should be successfull' do
+      get :show, :id => @user
+      response.should be_success
+    end
+
+    it 'should find the right user' do
+      get :show, :id => @user
+      assigns(:user).should == @user
+    end
+  end
+
   describe "GET 'new'" do
-    
+
     it "should be successfull" do
       visit '/users/new'
       response.should be_success
@@ -14,7 +31,7 @@ describe UsersController do
       visit '/users/new'
       page.should have_selector('title', :text => 'Sign up')
     end
-    
+
   end
 
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name "Aman"
+    email "aman@aman.com"
+    password "password"
+    password_confirmation "password"
+  end
+end
