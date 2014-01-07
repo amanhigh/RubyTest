26af9750156fd76784b9aa3c5c38310c5fef7b40
Amanpreet Singh <amanpreet.singh@flipkart.com>
2014-01-08 02:07:22 +0530

    Added feed.

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -6,11 +6,17 @@ class MicropostsController < ApplicationController
     if @micropost.save
       redirect_to root_path, :flash => {:success => ' Micropost created!'}
     else
+      @feed_items = current_user.feed.paginate(:page => params[:id], :per_page => 10)
       render 'pages/home'
     end
   end
 
   def destroy
-
+    post = Micropost.find_by_id(params[:id])
+    if post && post.destroy
+      redirect_to root_path, :flash => {:success => 'Micropost deleted'}
+    else
+      redirect_to root_path, :flash => {:error => 'Micropost deletion failed'}
+    end
   end
 end
