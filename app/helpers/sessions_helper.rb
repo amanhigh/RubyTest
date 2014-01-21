42ae4cfc2d696e6f705f876270fe978c48af030a
Amanpreet Singh <amanpreet.singh@flipkart.com>
2014-01-21 12:03:38 +0530

    Refactored, removed unnecessary setter.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,10 +3,6 @@ module SessionsHelper
     remember_token = User.new_remember_token
     cookies.permanent[:remember_token] = remember_token
     user.update_attribute(:remember_token, User.encrypt(remember_token))
-    self.current_user = user
-  end
-
-  def current_user=(user)
     @current_user = user
   end
 
@@ -22,7 +18,7 @@ module SessionsHelper
   def sign_out
     current_user.update_attribute(:remember_token, User.new_remember_token)
     cookies.delete(:remember_token)
-    self.current_user = nil
+    @current_user = nil
   end
 
   private
