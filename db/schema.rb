209041459200abc1b3fe0fcbdca0630f36717004
Amanpreet Singh <coool.aman@gmail.com>
2015-03-27 20:49:58 +0530

    Migrated Schema

--- db/schema.rb
+++ db/schema.rb
@@ -43,5 +43,6 @@ ActiveRecord::Schema.define(version: 20140108190348) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
